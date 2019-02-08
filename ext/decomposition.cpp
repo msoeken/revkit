ca785cb344872f4e118fbbe85650e8680dacc6cf
Mathias Soeken <mathias.soeken@epfl.ch>
2019-02-08 17:06:29 +0100

    Add decomposition.

--- ext/decomposition.cpp
+++ ext/decomposition.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 
+#include <tweedledum/algorithms/decomposition/barenco.hpp>
 #include <tweedledum/algorithms/decomposition/dt.hpp>
 
 #include "types.hpp"
@@ -11,7 +12,14 @@ namespace revkit
 
 void decomposition( py::module m )
 {
-  m.def( "dt_decomposition", &tweedledum::dt_decomposition<netlist_t>, "Direct Toffoli decomposition" );
+  using namespace py::literals;
+
+  m.def( "barenco_decomposition", []( netlist_t const& circ, unsigned controls_threshold ) {
+    tweedledum::barenco_params params;
+    params.controls_threshold = controls_threshold;
+    return tweedledum::barenco_decomposition<netlist_t>( circ, params );
+  }, "Barenco decomposition", "circ"_a, "controls_threshold"_a = 2u );
+  m.def( "dt_decomposition", &tweedledum::dt_decomposition<netlist_t>, "Direct Toffoli decomposition", "circ"_a );
 }
 
 } // namespace revkit
