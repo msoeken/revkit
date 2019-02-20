884d0683724304cb9d7f223cca2592e4b93eb1c7
Mathias Soeken <mathias.soeken@epfl.ch>
2019-02-20 14:17:34 +0100

    Setup script.

--- setup.py
+++ setup.py
@@ -7,24 +7,42 @@ import sys
 
 base_path = os.path.dirname(__file__)
 
-def pybind_include():
-  import pybind11
-  return pybind11.get_include()
+class get_pybind_include(object):
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
 
 ext_modules = [
   Extension(
     '_revkit',
-    glob.glob(os.path.join(base_path, 'ext', '*.cpp')),
+    glob.glob(os.path.join(base_path, 'ext', '*.cpp')) +
+    glob.glob(os.path.join(base_path, 'lib/abcsat', '*.cpp')),
     include_dirs=[
-      pybind_include(),
+      get_pybind_include(),
+      get_pybind_include(user=True),
+      os.path.join(base_path, 'lib', 'abcsat'),
+      os.path.join(base_path, 'lib', 'caterpillar'),
       os.path.join(base_path, 'lib', 'easy'),
+      os.path.join(base_path, 'lib', 'ez'),
       os.path.join(base_path, 'lib', 'fmt'),
       os.path.join(base_path, 'lib', 'glucose'),
       os.path.join(base_path, 'lib', 'kitty'),
+      os.path.join(base_path, 'lib', 'lorina'),
+      os.path.join(base_path, 'lib', 'mockturtle'),
+      os.path.join(base_path, 'lib', 'percy'),
+      os.path.join(base_path, 'lib', 'rang'),
+      os.path.join(base_path, 'lib', 'sparsepp'),
       os.path.join(base_path, 'lib', 'tweedledum')
     ],
     define_macros=[
-      ('FMT_HEADER_ONLY', '1')
+      ('FMT_HEADER_ONLY', '1'),
+      ('DISABLE_NAUTY', '1'),
+      ('LIN64', '1'),
+      ('ABC_NAMESPACE', 'pabc'),
+      ('ABC_NO_USE_READLINE', '1')
     ],
     language='c++'
   )
