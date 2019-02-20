d945a9cf060940b37251062c8f50f327e404bd88
Mathias Soeken <mathias.soeken@epfl.ch>
2019-02-20 16:25:51 +0100

    Use more generic gate type.

--- ext/types.hpp
+++ ext/types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <caterpillar/stg_gate.hpp>
 #include <kitty/dynamic_truth_table.hpp>
 #include <tweedledum/gates/mcmt_gate.hpp>
 #include <tweedledum/networks/netlist.hpp>
@@ -8,7 +9,7 @@ namespace revkit
 {
 
 using truth_table_t = kitty::dynamic_truth_table;
-using netlist_t = tweedledum::netlist<tweedledum::mcmt_gate>;
+using netlist_t = tweedledum::netlist<caterpillar::stg_gate>;
 using gate_t = netlist_t::gate_type;
 
 }
